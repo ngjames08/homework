71d11af8b4cd7414feb7ccc2ee11d769b25ad7da
ngjames08 <54019194+ngjames08@users.noreply.github.com>
2020-02-20 00:21:21 -0800

    D3 Homework

---
+++ D3 Homework - Data Journalism and D3/assets/js/app.js
@@ -0,0 +1,80 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+  };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.select("body").append("div").attr("class").style("opacity", 0);
+
+d3.csv("data.csv", function(err, healthData) {
+    if (err) throw err;
+  console.log(healthData)
+
+  healthData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+});
+
+var xLinearScale = d3.scaleLinear().range([0, width]);
+var yLinearScale = d3.scaleLinear().range([height, 0]);
+
+var bottomAxis = d3.axisBottom(xLinearScale);
+var leftAxis = d3.axisLeft(yLinearScale);
+
+var xMin;
+var xMax;
+var yMin;
+var yMax;
+
+xMin = d3.min(healthData, function(data) {
+    return data.healthcare;
+});
+
+xMax = d3.max(healthData, function(data) {
+    return data.healthcare;
+});
+
+yMin = d3.min(healthData, function(data) {
+    return data.poverty;
+});
+
+yMax = d3.max(healthData, function(data) {
+    return data.poverty;
+});
+
+xLinearScale.domain([xMin, xMax]);
+yLinearScale.domain([yMin, yMax]);
+console.log(xMin);
+console.log(yMax);
+
+chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+chartGroup.append("g")
+    .call(leftAxis);
+
+var circlesGroup = chartGroup.selectAll("circle")
+  .data(healthData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.healthcare +1.5))
+  .attr("cy", d => yLinearScale(d.poverty +0.3))
+  .attr("r", "12")
+  .attr("fill", "blue")
+  .attr("opacity", .5);
